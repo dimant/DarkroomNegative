264d372171086b2dfb38179f1856010acea29d34
Diman Todorov <diman.todorov@outlook.com>
2016-03-27 22:54:16 -0700

    added about activity

--- app/src/main/java/com/dtodorov/darkroomnegative/activities/MainActivity.java
+++ app/src/main/java/com/dtodorov/darkroomnegative/activities/MainActivity.java
@@ -8,6 +8,8 @@ import android.graphics.Bitmap;
 import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.SeekBar;
@@ -28,7 +30,6 @@ import com.dtodorov.darkroomnegative.services.ClapDetector;
 import com.dtodorov.darkroomnegative.services.Exposer;
 import com.dtodorov.darkroomnegative.services.FullScreen;
 import com.dtodorov.darkroomnegative.services.IClapDetector;
-import com.dtodorov.darkroomnegative.services.IFullScreen;
 import com.dtodorov.darkroomnegative.services.Toaster;
 
 
@@ -176,6 +177,30 @@ public class MainActivity extends AppCompatActivity {
         exposureTimeControl.setProgress(exposureTime);
         _clapDetector.setClapListener(_mainController);
         _clapDetector.start();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_about) {
+            Intent intent = new Intent(this, AboutActivity.class);
+            startActivity(intent);
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 
     public void pickImage(View view) {
