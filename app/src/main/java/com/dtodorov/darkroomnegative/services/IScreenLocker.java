b98100997b1a36f8c51f13cdb4e6c3eadcd6f1fd
Diman Todorov <diman.todorov@outlook.com>
2016-03-28 21:12:44 -0700

    locking screen

---
+++ app/src/main/java/com/dtodorov/darkroomnegative/services/IScreenLocker.java
@@ -0,0 +1,9 @@
+package com.dtodorov.darkroomnegative.services;
+
+/**
+ * Created by diman on 3/28/2016.
+ */
+public interface IScreenLocker {
+    public void setKeepScreenOn();
+    public void clearKeepScreenOn();
+}
